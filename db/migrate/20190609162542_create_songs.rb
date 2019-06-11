79450fdecb060a1c3b384b8cdee562dde58cbb81
unknown <aaochoa4@gmail.com>
2019-06-10 19:09:15 -0500

    finishing models

--- db/migrate/20190609162542_create_songs.rb
+++ db/migrate/20190609162542_create_songs.rb
@@ -1,7 +1,7 @@
 class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
-      t.references :artist, foreign_key: true
+      t.references :album, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2019_06_09_162640) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "albums", force: :cascade do |t|
-    t.integer "artist_id"
+    t.bigint "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
@@ -36,7 +39,7 @@ ActiveRecord::Schema.define(version: 2019_06_09_162640) do
   end
 
   create_table "songs", force: :cascade do |t|
-    t.integer "artist_id"
+    t.bigint "album_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
@@ -45,7 +48,9 @@ ActiveRecord::Schema.define(version: 2019_06_09_162640) do
     t.string "duration_ms"
     t.boolean "explicit"
     t.string "spotify_id"
-    t.index ["artist_id"], name: "index_songs_on_artist_id"
+    t.index ["album_id"], name: "index_songs_on_album_id"
   end
 
+  add_foreign_key "albums", "artists"
+  add_foreign_key "songs", "albums"
 end

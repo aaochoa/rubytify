24d068bf124634e662fbf8efa05af10fd99e12ee
unknown <aaochoa4@gmail.com>
2019-06-13 21:24:37 -0500

    Minor identation change

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace 'v1' do
-
       get 'artists', to: 'artists#index'
       get 'artists/:id/albums', to: 'artists#get_albums'
       get 'albums/:id/songs', to: 'albums#get_songs'
       get 'genres/:genre_name/random_song', to: 'genres#song_by_genre'
-      
     end
   end
 
